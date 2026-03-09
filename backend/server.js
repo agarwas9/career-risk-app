b3c0ab60d78966a9457eeea961919a8767355339
agarwas9 <agarwalsharad38@gmail.com>
2026-03-09 22:17:22 +0100

    Test Jenkins CI/CD deployment

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ const { Pool } = require('pg');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+console.log("Deployed by Jenkins CI/CD");
+
 const pool = new Pool({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
